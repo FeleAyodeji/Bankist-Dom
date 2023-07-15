33fb094eb1aabc8b52cf5590aade49310a006a1b
FeleAyodeji <feledeji@gmail.com>
2023-07-15 17:09:58 +0100

    event delegation

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -22,6 +25,7 @@ const closeModal = function () {
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
+
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
@@ -30,8 +34,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+////////////////////////////////////////////////////////
+//scrolling
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect(); // getting the cordinates of the element er are scrolling to
@@ -45,7 +49,36 @@ btnScrollTo.addEventListener('click', function (e) {
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   ); */
-
-  //scrolling
   section1.scrollIntoView({ behaviour: 'smooth' });
+});
+
+///////////////////////////////////////////////////////////////////
+
+//Page navigation
+
+// by using foreach on the nav buttons to navigate, this procedure isnt 100 accurate
+
+/* document.querySelectorAll('nav__link').forEach(function (el) {
+  el.addEventListener('clcik', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behaviour: 'smooth' });
+  });
+}); */
+
+//By using event delegation
+
+//1. add event listener to common parent element
+//2. determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behaviour: 'smooth',
+    }); //The scrollIntoView() method is a built-in JavaScript function that scrolls the element into the visible area of the browser window. It can be used to automatically scroll to a specific element on a web page.
+  }
 });
